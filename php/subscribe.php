b64a41ab7f18b8a78c9cf8dcfe66b4c9e5ca8034
automatedcashiertech3 <31944336+automatedcashiertech3@users.noreply.github.com>
2017-10-07 13:03:28 -0400

    Add files via upload

---
+++ php/subscribe.php
@@ -0,0 +1,29 @@
+<?php
+
+	// MailChimp
+	$APIKey = 'eeb2518b8c9768fd0e5d630280b2b302-us16';
+	$listID = '02542af84b';
+
+	$email   = $_POST['email'];
+
+	require_once('inc/MCAPI.class.php');
+
+	$api = new MCAPI($APIKey);
+	$list_id = $listID;
+
+	if($api->listSubscribe($list_id, $email) === true) {
+		$sendstatus = 1;
+		$message = '<div class="alert alert-success subscription-success" role="alert"><strong>Success!</strong> Check your email to confirm sign up.</div>';
+	} else {
+		$sendstatus = 0;
+		$message = '<div class="alert alert-danger subscription-error" role="alert"><strong>Error:</strong> ' . $api->errorMessage.'</div>';
+	}
+
+	$result = array(
+		'sendstatus' => $sendstatus,
+		'message' => $message
+	);
+
+	echo json_encode($result);
+
+?>
